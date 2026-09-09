df58b4a30ded14145fbdcc08a2615fc632f012bf
Daniel Silva <djsilva@gmx.com>
2026-09-09 23:28:31 +0100

    style: fix lint issues

--- apps/api/src/adapters/entrypoints/v1/routes.py
+++ apps/api/src/adapters/entrypoints/v1/routes.py
@@ -4,7 +4,6 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, Query, Request, Response, status
 from fastapi.responses import FileResponse
-
 from fastapi.security import OAuth2PasswordBearer
 from src.adapters.entrypoints.v1.models.authentication import LoginRequest
 from src.adapters.entrypoints.v1.models.feeds import (
